e96355a030873859c63238dd1aef5800f4110006
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-12-09 10:30:07 +0800

    Create index.js
    "It will make imports cleaner."

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Nav from './navbar';
+import Test1 from './Test1';
+import Test2 from './Test2';
+import Test3 from './Test3';
+import Test4 from './Test4';
+
+export {
+    Nav,
+    Test1,
+    Test2,
+    Test3,
+    Test4
+}
