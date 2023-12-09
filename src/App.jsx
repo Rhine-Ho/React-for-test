81ba2a64102437e9ece0733696c152ceb099c0c8
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-12-09 11:08:45 +0800

    set routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-//import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Nav, Home, Test1, Test2, Test3, Test4} from "./components";
 
 
 function App() {
@@ -6,7 +8,23 @@ function App() {
 
   return (
     <>
-    
+    <Router>
+      
+        <header>
+          <Nav />
+        </header>
+      
+        <main>
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Test1" element={<Test1 />} />
+            <Route path="/Test2" element={<Test2 />} />
+            <Route path="/Test3" element={<Test3 />} />
+            <Route path="/Test4" element={<Test4 />} />
+          </Routes>
+       </main>
+      
+    </Router>
     </>
   )
 }
