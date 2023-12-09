b80da73bc8e0d5b47f1b962e612eed7a3b49cb0d
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-12-09 11:19:45 +0800

    Set home page.

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import nickname from "../assets/nickname.ico";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-400">
+      <div className="bg-blue-500 w-1/3 shadow-md rounded-lg p-8">
+        <div className="flex flex-col items-center">
+          <img
+            src={nickname}
+            alt="Avatar"
+            className="w-32 h-32 rounded-full mb-4"
+          />
+          <h2 className="text-xl font-bold">Hello.👋🏻 I'm Rhine.</h2>
+          <p className="text-white font-bold drop-shadow-md">Welcome.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
