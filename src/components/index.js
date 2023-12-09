88e9e26ed4b5de5ad8cff081950a1bfd451ce7ab
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-12-09 11:10:09 +0800

    add Home component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Nav from './navbar';
+import Home from './Home';
 import Test1 from './Test1';
 import Test2 from './Test2';
 import Test3 from './Test3';
@@ -6,6 +7,7 @@ import Test4 from './Test4';
 
 export {
     Nav,
+    Home,
     Test1,
     Test2,
     Test3,
