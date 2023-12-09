28e917b16c73c206304b5db40af41f79c9b46d2d
Rhine-Ho <107762170+Rhine-Ho@users.noreply.github.com>
2023-12-09 10:28:30 +0800

    constants set

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        id: "Home",
+        title: "Home",
+      },
+    {
+      id: "T1",
+      title: "Test1",
+    },
+    {
+      id: "T2",
+      title: "Test2",
+    },
+    {
+      id: "T3",
+      title: "Test3",
+    },
+    {
+      id: "T4",
+      title: "Test4",
+    },
+  ];
